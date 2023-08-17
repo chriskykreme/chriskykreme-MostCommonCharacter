04324db31bbde536b0f748d74b485e5d6593d9c0
Christopher King <king.cdk@gmail.com>
2023-08-17 22:02:39 +0000

    Added logic to MostCommonCharacter file

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +9,41 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        // Create a HashMap and add all of str's characters to it.
+        HashMap<Character, Integer> characterCount = new HashMap<Character, Integer>();
+        Set<Character> keys = new HashSet<Character>();
+
+        // Add letters of string to HashMap
+        // Populate Set with keys of HashMap
+        // Ensure no character is added more than once
+        for (int i = 0; i < str.length(); i++) {
+            
+            // If chracter is already in keys, it's already in the HashMap. 
+            // Increment its count and continue through the string
+            Character currentChar = str.charAt(i);
+            if (keys.contains(currentChar)) {
+                characterCount.put(currentChar, characterCount.get(currentChar) + 1);
+                continue;
+            }
+
+            // If here the character is not present in HashMap, add it.
+            characterCount.put(currentChar, 0);
+
+            // Add the character to keys in case it occurs again
+            keys.add(currentChar);
+        }
+
+        // Determine which character occurs most
+        int largest = characterCount.get(str.charAt(0));
+        String largestKey = ""; 
+        for (char key : keys) {
+            if (characterCount.get(key) >= largest) {
+                largest = characterCount.get(key);
+                largestKey = "";
+                largestKey = largestKey + key;
+            }
+        }
+
+        return largestKey.charAt(0);
     }
 }
